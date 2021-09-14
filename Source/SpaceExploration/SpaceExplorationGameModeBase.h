91b542854e742e3e8ebc9d6a3d32595c4adbc6cc
ShyAway1800 <154harshvardhan@gmail.com>
2021-09-14 12:33:37 +0530

    first commit
    first commit

---
+++ Source/SpaceExploration/SpaceExplorationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpaceExplorationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEEXPLORATION_API ASpaceExplorationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
